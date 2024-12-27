6da0b57a56b81af3c29677ff0ecc72c224c4c5f7
AbylayNurtayev <krutyev6@gmail.com>
2024-12-27 18:44:54 +0500

    dsad

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom';
 import StartPage from './pages/StartPage';
 import { useEffect } from 'react';
-function App() {
 
+function App() {
   useEffect(() => {
     if (window.Telegram && window.Telegram.WebApp) {
       const tg = window.Telegram.WebApp;
+      console.log("Инициализируем Telegram WebApp API...");
       tg.ready(); // Инициализация WebApp API
-      tg.expand(); // Расширение на весь экран
+      tg.expand(); // Полноэкранный режим
+    } else {
+      console.error("Telegram WebApp API недоступен");
     }
   }, []);
 
   return (
     <div className="App flex flex-col justify-start items-center w-[100%]">
       <Routes>
-        <Route element={<StartPage/>} index />
-
+        <Route element={<StartPage />} index />
       </Routes>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
-document.addEventListener('DOMContentLoaded', () => {
-  Telegram.WebApp.ready()
-  Telegram.WebApp.expand()
-})
+// document.addEventListener('DOMContentLoaded', () => {
+//   Telegram.WebApp.ready()
+//   Telegram.WebApp.expand()
+// })
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
