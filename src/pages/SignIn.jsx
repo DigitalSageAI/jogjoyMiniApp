e382310e0558b114a355bca4c85c85784a922c23
AbylayNurtayev <krutyev6@gmail.com>
2024-12-28 11:36:52 +0500

    fdasfdas

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -2,15 +2,27 @@ import React, { useState } from 'react'
 import Input from '../components/ui/Input'
 import Button from '../components/ui/Button'
 import { useNavigate } from 'react-router-dom'
+import axios from '../axios'
 
 function SignIn() {
     const [email, setEmail] = useState()
     const [password, setPassword] = useState()
+    const [error, setError] = useState(false)
 
     const navigate = useNavigate()
 
     const sendInfo = () => {
-        console.log('info sending');
+        axios.post('/login', {
+            email, password
+        })
+        .then(res => res.data)
+        .then((data) => {
+            if(data){
+                setError(false)
+                navigate('/home')
+            }
+        })
+        .catch((err) => setError(true))
         
     }
     console.log(email, password);
@@ -20,6 +32,9 @@ function SignIn() {
         <p className='font-syne text-[21px] text-white font-semibold mt-[79px] w-[90%] text-left'>Sign in</p>
         <Input value={email} onChange={(e) => setEmail(e.target.value)} className="mt-4" placeholder={'Email Address'}></Input>
         <Input value={password} onChange={(e) => setPassword(e.target.value)} className="mt-4" placeholder={'Your password'}></Input>
+        {
+            error &&  <p className='text-red-500 text-sm mt-1 w-[90%] text-left'>not correct email or password</p>
+        }
         <Button className="mt-4" onClick={sendInfo}>Get started</Button>
         <p style={{ color: "rgba(235, 235, 245, 0.6)" }} className='font-sans font-[14px] mt-[11px] w-[90%]'>By clicking “Get started”, you agree to the <span className='text-white'>Terms</span> and <span className='text-white'>Privacy policy.</span></p>
         <img src="/icons/Login.png" alt="login" className='mt-[32px] w-[90%]'/>
