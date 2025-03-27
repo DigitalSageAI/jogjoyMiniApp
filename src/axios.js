605331f30ae8a8c6a70b1a5a364dd90d60a50c29
DigitalSageAI <135725779+DigitalSageAI@users.noreply.github.com>
2025-03-27 18:29:11 +0200

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
     // baseURL: "http://localhost:3001"
-    baseURL: "https://jog-joy-miniapp-back-production.up.railway.app"
+    baseURL: "https://jogjoyminiappback-production.up.railway.app"
 });
 
 instance.interceptors.request.use((config) => {
